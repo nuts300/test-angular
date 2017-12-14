85af615101c15f84402bf1605a1da0543246c142
Inatsu, Yoshinari | COED <ts-yoshinari.inatsu@rakuten.com>
2017-12-14 11:31:21 +0900

    Add app-routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HerosComponent }      from './heros/heros.component';
+
+const routes: Routes = [
+  { path: 'heroes', component: HerosComponent }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [ RouterModule ],
+  declarations: []
+})
+export class AppRoutingModule { }
